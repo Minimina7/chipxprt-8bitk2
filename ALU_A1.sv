aee13c7536d8811eb288f94129fa85c37d6a41d1
Fadhel-f22 <fadel.f1422@gmail.com>
2024-11-22 20:19:57 +0300

    modified block

--- ALU_A1.sv
+++ ALU_A1.sv
@@ -2,32 +2,37 @@
 
 
 module ALU_A1 #(parameter bits = 8)(
-input logic [bits : 0]RA,
-input logic [bits : 0]RB,
+input logic [bits: 0]RA,
+input logic [bits: 0]RB,
 input logic s,
-output logic [bits-1 : 0]out,
+output logic [bits : 0]out,
 output logic carry_out,
 output logic zero_flag
     );
     
-always @ (s)
+
+    
+always @ (*)
 begin
-    if (s==1)
+    if (s==0)
     begin
         out = RA + RB ;
-        if (out[bits] == 1'b1)
-            carry_out = 1;
-        else
-            carry_out = 0;
+        carry_out = out[bits];
+        if (out[bits - 1:0] == 0)
+            zero_flag = 1;
+        else 
+            zero_flag = 0;
     end
-    else if (s==0)
+    else if (s==1)
     begin
         out = RA - RB ;
-        if (out == 0)
+        carry_out = 0;
+        if (out[bits - 1:0] == 0)
             zero_flag = 1;
         else
             zero_flag = 0;
     end
+
 end
 
 
